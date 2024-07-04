cad8c05dba82eef49af3c53119c00a214844ce66
ikmq0 <71012568+ikmq0@users.noreply.github.com>
2024-07-04 15:12:19 +0300

    Completed keyboard keys event listeners, and I've made the screen flexable to fit the numbers

--- script.js
+++ script.js
@@ -1,31 +1,40 @@
+// Variables Section : 
+
 const display = document.querySelector('.screen');
 let lastOperator = null;
 let firstOperand = null;
+const numbers = [0,1,2,3,4,5,6,7,8,9]
+const operations = ['*','-','+','/']
 
+// EventListeners for KeyBoard & Mouse
 document.addEventListener('click', (e) => {
+
   const currentElement = e.target;
-
+  const currentElementValue = currentElement.getAttribute('value');
   // Display & Visual Ability 
-  if(currentElement.getAttribute('value') === "clear") clearDisplay();
-  if(currentElement.classList.contains('number')) appendNumber(currentElement.getAttribute('value'));
-  if(currentElement.getAttribute('value') === ".") addDecimal();
-
-  if(currentElement.getAttribute('value') === "*") handleOperator('*');
-  if(currentElement.getAttribute('value') === "-") handleOperator('-');
-  if(currentElement.getAttribute('value') === "+") handleOperator('+');
-  if(currentElement.getAttribute('value') === "percent") percent();
-  if(currentElement.getAttribute('value') === "=") equals();
-  if(currentElement.getAttribute('value') === "/") handleOperator('/');
-  if(currentElement.getAttribute('value') === "sign") changeSign();
-
+  if(currentElementValue === "clear" || display.textContent == "NaN") clearDisplay();
+  if(currentElement.classList.contains('number')) appendNumber(currentElementValue);
+  if(operations.includes(currentElementValue)) handleOperator(currentElementValue);
+  if(currentElementValue === ".") addDecimal();
+  if(currentElementValue === "percent") percent();
+  if(currentElementValue === "=") equals()
+  if(currentElementValue === "sign") changeSign();
 });
 
-document.addEventListener('keyup', (e)=> {
-  display.textContent = "meow"
+document.addEventListener('keydown', (e) =>  {
+
+  if(numbers.includes(parseInt(e.key))) appendNumber(e.key);
+  if(operations.includes(e.key)) handleOperator(e.key);
+  if(e.key === ".") addDecimal();
+  if(e.key === "%") percent();
+  if(e.key === "Enter") equals();
+  if(e.key === "c") clearDisplay();
+  if(e.key === "Backspace") deleteLast();
 })
 
+// Calculator Functions
 function appendNumber(number){
-  if (display.textContent === "0" ) {
+  if (display.textContent == "0" ) {
     display.textContent = number;
     return;
   } else if (display.textContent.length < 8 ) { 
@@ -33,6 +42,11 @@ function appendNumber(number){
   }
 }
 
+function deleteLast(){
+  if (!display.textContent == "0"){
+    display.textContent = display.textContent.substring(0, display.textContent.length - 1);
+  }
+}
 function addDecimal() {
   if(!display.textContent.includes('.')) display.textContent += '.';
 }
@@ -68,7 +82,7 @@ function equals() {
   }
 }
 
-function operate(first, second , operator) { 
+function operate(first, second, operator) { 
   if(operator === '*') return first * second;
   if(operator === '+') return first + second;
   if(operator === '-') return first - second;
@@ -84,4 +98,3 @@ function changeSign() {
   let current = parseFloat(display.textContent);
   updateDisplay(current * -1);
 }
-  
